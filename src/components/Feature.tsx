eb34ddd7d2728508805b97ff38813713587ca0dd
Joshua Ellis <joshua@wideeyecreative.com>
2025-03-05 11:06:50 -0500

    prettier

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -14,16 +14,18 @@ export const FeatureCard = ({
   nodeCount: number;
 }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
-	const [confirmDelete, setConfirmDelete] = useState(false);
+  const [confirmDelete, setConfirmDelete] = useState(false);
 
   const handleDelete = () => {
-		setConfirmDelete(true);
-		setTimeout(() => {
-			setConfirmDelete(false);
-		}, 3000);
-		if (confirmDelete) {
-			setFeatures((state: Feature[]) => state.filter((f) => f.id !== feature.id));
-		}
+    setConfirmDelete(true);
+    setTimeout(() => {
+      setConfirmDelete(false);
+    }, 3000);
+    if (confirmDelete) {
+      setFeatures((state: Feature[]) =>
+        state.filter((f) => f.id !== feature.id)
+      );
+    }
   };
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -59,7 +61,7 @@ export const FeatureCard = ({
       {
         pluginMessage: {
           type: 'FOCUS_NODE',
-          nodeId: nodeId
+          nodeId: nodeId,
         },
       },
       '*'
@@ -79,15 +81,17 @@ export const FeatureCard = ({
     );
   };
 
-	const handleUntagTextNode = (id: string) => {
-		setFeatures((state) =>
-			state.map((f) =>
-				f.id === feature.id ? { ...f, notes: f.notes.filter((n) => n.nodeId !== id) } : f
-			)
-		);
-	};
+  const handleUntagTextNode = (id: string) => {
+    setFeatures((state) =>
+      state.map((f) =>
+        f.id === feature.id
+          ? { ...f, notes: f.notes.filter((n) => n.nodeId !== id) }
+          : f
+      )
+    );
+  };
 
-	const handleImageDelete = (index: number) => {
+  const handleImageDelete = (index: number) => {
     setFeatures((state) =>
       state.map((f) =>
         f.id === feature.id
@@ -99,111 +103,136 @@ export const FeatureCard = ({
 
   return (
     <article className="feature shadow-sm rounded-md p-2">
-			<AnimatePresence>
-				<div key={feature.id + '-header'} className='flex flex-row gap-4 items-center justify-between h-12'>
-					{isCollapsed ? (
-						<h2 className="text-lg font-semibold">{feature.title.length > 0 ? (feature.title) : (<span className='text-gray-500'>[Untitled]</span>)}</h2>
-					) : (
-						<input
-							type="text"
-							value={feature.title}
-							onChange={handleTitleChange}
-							placeholder="Feature Title"
-							className="border-1 border-black p-2 border-r-0 border-t-0 border-l-0 text-lg font-semibold flex-1"
-						/>
-					)}
-					<div className='flex flex-row gap-4'>
-						<Button onClick={handleDelete} type="minus" color="warn">
-							{confirmDelete ? 'Confirm Delete' : 'Delete'}
-						</Button>
-						<Button onClick={() => setIsCollapsed(!isCollapsed)} type={isCollapsed ? 'plus' : 'minus'}>
-							{isCollapsed ? 'Expand' : 'Collapse'}
-						</Button>
-					</div>
-				</div>
-				<Collapse open={!isCollapsed} className="flex flex-col gap-2">
-					<div className="flex flex-row gap-2 w-full *:flex-1/2 mt-4 mb-4">
-						<div className="flex flex-row gap-2 items-center">
-							<label htmlFor="high-estimate" className="font-semibold">
-								High Estimate
-							</label>
-							<input
-								type="number"
-								id="high-estimate"
-								value={feature.highEstimate}
-								onChange={handleHighEstimateChange}
-								className="border-1 border-black p-2 flex-1 border-r-0 border-t-0 border-l-0"
-							/>
-						</div>
-						<div className="flex flex-row gap-2 items-center">
-							<label htmlFor="low-estimate" className="font-semibold">
-								Low Estimate
-							</label>
-							<input
-								type="number"
-								id="low-estimate"
-								value={feature.lowEstimate}
-								onChange={handleLowEstimateChange}
-								className="border-1 border-black p-2 flex-1 border-r-0 border-t-0 border-l-0"
-							/>
-						</div>
-					</div>
+      <AnimatePresence>
+        <div
+          key={feature.id + '-header'}
+          className="flex flex-row gap-4 items-center justify-between h-12"
+        >
+          {isCollapsed ? (
+            <h2 className="text-lg font-semibold">
+              {feature.title.length > 0 ? (
+                feature.title
+              ) : (
+                <span className="text-gray-500">[Untitled]</span>
+              )}
+            </h2>
+          ) : (
+            <input
+              type="text"
+              value={feature.title}
+              onChange={handleTitleChange}
+              placeholder="Feature Title"
+              className="border-1 border-black p-2 border-r-0 border-t-0 border-l-0 text-lg font-semibold flex-1"
+            />
+          )}
+          <div className="flex flex-row gap-4">
+            <Button onClick={handleDelete} type="minus" color="warn">
+              {confirmDelete ? 'Confirm Delete' : 'Delete'}
+            </Button>
+            <Button
+              onClick={() => setIsCollapsed(!isCollapsed)}
+              type={isCollapsed ? 'plus' : 'minus'}
+            >
+              {isCollapsed ? 'Expand' : 'Collapse'}
+            </Button>
+          </div>
+        </div>
+        <Collapse open={!isCollapsed} className="flex flex-col gap-2">
+          <div className="flex flex-row gap-2 w-full *:flex-1/2 mt-4 mb-4">
+            <div className="flex flex-row gap-2 items-center">
+              <label htmlFor="high-estimate" className="font-semibold">
+                High Estimate
+              </label>
+              <input
+                type="number"
+                id="high-estimate"
+                value={feature.highEstimate}
+                onChange={handleHighEstimateChange}
+                className="border-1 border-black p-2 flex-1 border-r-0 border-t-0 border-l-0"
+              />
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <label htmlFor="low-estimate" className="font-semibold">
+                Low Estimate
+              </label>
+              <input
+                type="number"
+                id="low-estimate"
+                value={feature.lowEstimate}
+                onChange={handleLowEstimateChange}
+                className="border-1 border-black p-2 flex-1 border-r-0 border-t-0 border-l-0"
+              />
+            </div>
+          </div>
 
-					<div className="flex flex-row gap-4 w-full *:flex-1/2 ">
-						{/* Text */}
+          <div className="flex flex-row gap-4 w-full *:flex-1/2 ">
+            {/* Text */}
 
-						<div className="flex flex-col gap-2 bg-gray-100 rounded-md p-2">
-							<div className="flex flex-row gap-2 w-full">
-								<Button onClick={() => handleTagNode('document')} type="plus">
-									Tag Document Note
-								</Button>
-							</div>
+            <div className="flex flex-col gap-2 bg-gray-100 rounded-md p-2">
+              <div className="flex flex-row gap-2 w-full">
+                <Button onClick={() => handleTagNode('document')} type="plus">
+                  Tag Document Note
+                </Button>
+              </div>
 
-							<div className="flex flex-col gap-2 w-full">
-								{feature.notes.map((note, index) => (
-									<div key={`note-${index}`} className="flex flex-col gap-2">
-										<div className="flex flex-row gap-2 w-full justify-between">
-											<Button onClick={() => handleImageFocus(note.nodeId)}>
-												Go to Frame
-											</Button>
-											<Button onClick={() => handleUntagTextNode(note.nodeId)} type="minus">
-												Delete
-											</Button>
-										</div>
-										<div className="w-full">
-											<div className="markdown-preview w-full h-auto min-h-[20px] p-2 max-h-[200px] overflow-y-auto border-1 border-gray-200 rounded-md">
-												<ReactMarkdown>{note.content}</ReactMarkdown>	
-											</div>
-										</div>
-									</div>
-								))}
-							</div>
-						</div>
+              <div className="flex flex-col gap-2 w-full">
+                {feature.notes.map((note, index) => (
+                  <div key={`note-${index}`} className="flex flex-col gap-2">
+                    <div className="flex flex-row gap-2 w-full justify-between">
+                      <Button onClick={() => handleImageFocus(note.nodeId)}>
+                        Go to Frame
+                      </Button>
+                      <Button
+                        onClick={() => handleUntagTextNode(note.nodeId)}
+                        type="minus"
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                    <div className="w-full">
+                      <div className="markdown-preview w-full h-auto min-h-[20px] p-2 max-h-[200px] overflow-y-auto border-1 border-gray-200 rounded-md">
+                        <ReactMarkdown>{note.content}</ReactMarkdown>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
 
-						{/* Images */}
-						<div className="flex flex-col gap-2 bg-gray-100 rounded-md p-2">
-							<div className="flex flex-col gap-2 w-full">
-								{feature.images.map((imgNode, index) => (
-									<div key={`img-${index}`} className="flex flex-col gap-2">
-										<div className="flex flex-row gap-2 w-full justify-between">
-											<Button onClick={() => handleImageFocus(imgNode.nodeId)}>
-												Go to Frame
-											</Button>
-											<Button onClick={() => handleImageDelete(index)} type="minus">
-												Delete
-											</Button>
-										</div>
-										<iframe src={imgNode.embedUrl} className="w-full h-auto"></iframe>
-									</div>
-								))}
-							</div>
-							<Button onClick={() => handleTagNode('image')} type="plus" disabled={nodeCount === 0}>
-								Tag New Frame
-							</Button>
-						</div>
-					</div>
-				</Collapse>
-			</AnimatePresence>
+            {/* Images */}
+            <div className="flex flex-col gap-2 bg-gray-100 rounded-md p-2">
+              <div className="flex flex-col gap-2 w-full">
+                {feature.images.map((imgNode, index) => (
+                  <div key={`img-${index}`} className="flex flex-col gap-2">
+                    <div className="flex flex-row gap-2 w-full justify-between">
+                      <Button onClick={() => handleImageFocus(imgNode.nodeId)}>
+                        Go to Frame
+                      </Button>
+                      <Button
+                        onClick={() => handleImageDelete(index)}
+                        type="minus"
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                    <iframe
+                      src={imgNode.embedUrl}
+                      className="w-full h-auto"
+                    ></iframe>
+                  </div>
+                ))}
+              </div>
+              <Button
+                onClick={() => handleTagNode('image')}
+                type="plus"
+                disabled={nodeCount === 0}
+              >
+                Tag New Frame
+              </Button>
+            </div>
+          </div>
+        </Collapse>
+      </AnimatePresence>
     </article>
   );
 };
